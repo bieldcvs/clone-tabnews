21ade9197fbc01d8768271ce635af708976e4edf
Gabriel de Carvalho <bieldcvs@gmail.com>
2026-08-03 16:06:28 +0000

    Adicionado os scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-function home(){
-    return <h1>Mooooo eu te amooooooo asdasdasd muitaoooooooo ❤️❤️❤️❤️ </h1>
+function home() {
+  return <h1>Mooooo eu te amooooooo asdasdasd muitaoooooooo ❤️❤️❤️❤️ </h1>;
 }
+
+function Teste() {
+  console.log("teste");
+}
+
 export default home;
